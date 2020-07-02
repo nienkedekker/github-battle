768ed80c8bda85393cbd04890d1718cc9b1c10ae
Nienke Dekker <nienke.dekker@nos.nl>
2020-07-02 13:57:00 +0200

    add copy webpack plugin for redirects

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 // CommonJS
 module.exports = {
 	entry: './app/index.js',
@@ -15,11 +17,11 @@ module.exports = {
 			{ test: /\.(css)$/, use: ['style-loader', 'css-loader'] },
 		],
 	},
-	mode: 'development',
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './app/index.html',
 		}),
+		new CopyPlugin({ patterns: [{ from: '_redirects' }] })
 	],
 	devServer: {
 		historyApiFallback: true,
