0a0c9af2404c7e6b0ba94a6cc0375dc4c3972323
Nienke Dekker <nienke.dekker@nos.nl>
2020-07-02 11:06:23 +0200

    finish react router

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { ThemeConsumer } from '../context/theme.js';
-
+import { NavLink } from 'react-router-dom';
+const activeStyle = {
+	color: 'red'
+};
 export default function Nav() {
-  return (
-    <ThemeConsumer>
-      {({ theme, toggleTheme }) => (
-        <nav className="row space-between">
-          <button
-            onClick={toggleTheme}
-            className="btn-clear"
-            style={{ fontSize: 30 }}
-          >
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
-  );
+	return (
+		<ThemeConsumer>
+			{({ theme, toggleTheme }) => (
+				<nav className="row space-between">
+					<ul className="row nav">
+						<li>
+							<NavLink to="/" exact activeStyle={activeStyle} className="nav-link">Popular</NavLink>
+						</li>
+						<li>
+							<NavLink to="/battle" activeStyle={activeStyle} className="nav-link">Battle</NavLink>
+						</li>
+					</ul>
+					<button
+						onClick={toggleTheme}
+						className="btn-clear"
+						style={{ fontSize: 30 }}
+					>
+						{theme === 'light' ? '🔦' : '💡'}
+					</button>
+				</nav>
+			)}
+		</ThemeConsumer>
+	);
 }
--- app/index.js
+++ app/index.js
@@ -1,35 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Popular from './components/Popular.js';
 import Battle from './components/Battle.js';
-import { ThemeProvider } from './context/theme.js';
+import Results from './components/Results.js';
 import Nav from './components/Nav.js';
+import { ThemeProvider } from './context/theme.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './index.css';
+
+const NoRouteMatch = ({ location }) => {
+	return (
+		<div>
+			No match for {location.pathname}
+		</div>
+	);
+};
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light',
-        }));
-      },
-    };
-  }
-  render() {
-    return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className="container">
-            <Nav />
-            <Popular />
-          </div>
-        </div>
-      </ThemeProvider>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			theme: 'light',
+			toggleTheme: () => {
+				this.setState(({ theme }) => ({
+					theme: theme === 'light' ? 'dark' : 'light',
+				}));
+			},
+		};
+	}
+	render() {
+		return (
+			<Router>
+				<ThemeProvider value={this.state}>
+					<div className={this.state.theme}>
+						<div className="container">
+							<Nav />
+							<Switch>
+								<Route path="/" exact component={Popular} />
+								<Route path="/battle" exact component={Battle} />
+								<Route path="/battle/results" component={Results} />
+								<Route component={NoRouteMatch} />
+							</Switch>
+						</div>
+					</div>
+				</ThemeProvider>
+			</Router>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
